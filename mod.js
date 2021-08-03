a228291965d8afaf5e8ca0eb94c0c8c3b757b0e1
Tom Wilson <twilson63@gmail.com>
2021-08-03 12:02:06 +0000

    feat: wired up mod.js (#3)

--- mod.js
+++ mod.js
@@ -1,9 +1,16 @@
+import { Datastore } from "./deps.js";
 import adapter from "./adapter.js";
 import PORT_NAME from "./port_name.js";
 
-export default () => ({
+export default (dbFile) => ({
   id: "queue",
   port: PORT_NAME,
-  load: () => ({}), // load env
+  load: () => {
+    if (!dbFile) {
+      throw new Error("DB FILE: required for queue adapter");
+    }
+    const db = new Datastore({ filename: dbFile, autoload: true });
+    return { db };
+  }, // load env
   link: (env) => (_) => adapter(env), // link adapter
 });
--- scripts/harness.sh
+++ scripts/harness.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-deno run --unstable -A ./test/hyper.js
+deno run --no-check --unstable -A ./test/hyper.js
