21307634cab83d6171dc2f5a6bd583dd955ae23a
Tyler Hall <tyler@hyper.io>
2022-04-19 18:21:20 +0000

    refactor: use HyperErr from hyper-utils

--- deps.js
+++ deps.js
@@ -3,3 +3,8 @@ export * as R from "https://cdn.skypack.dev/ramda@0.28.0";
 export { default as Queue } from "https://deno.land/x/queue@1.2.0/mod.ts";
 export { default as Datastore } from "https://deno.land/x/dndb@0.3.3/mod.ts";
 export { join } from "https://deno.land/std@0.135.0/path/mod.ts";
+
+export {
+  HyperErr,
+  isHyperErr,
+} from "https://x.nest.land/hyper-utils@0.1.0/hyper-err.js";
