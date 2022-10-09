8322518fb161edc7890f70184dc001d6351b8cea
Tyler Hall <tyler@hyper.io>
2022-10-09 21:09:36 +0000

    test: add more coverage and reorganize test structures #15

--- mod.test.js
+++ mod.test.js
@@ -3,29 +3,32 @@ import { assert, validateFactorySchema } from "./dev_deps.js";
 import factory from "./mod.js";
 
 Deno.test({
-  name: "should be a valid schema",
+  name: "mod",
   sanitizeResources: false,
   sanitizeOps: false,
-  fn: () => {
-    assert(validateFactorySchema(factory()));
-  },
-});
+  fn: async (t) => {
+    await t.step({
+      name: "should be a valid schema",
+      fn: () => {
+        assert(validateFactorySchema(factory()));
+      },
+    });
 
-Deno.test({
-  name: "should accept a string",
-  sanitizeResources: false,
-  sanitizeOps: false,
-  fn: () => {
-    assert(factory("test/hyper-foo.db").load());
-  },
-});
+    await t.step("load", async (t) => {
+      await t.step({
+        name: "should accept a string",
+        fn: () => {
+          assert(factory("test/hyper-foo.db").load());
+        },
+      });
 
-Deno.test({
-  name: "should accept an object with defaults",
-  sanitizeResources: false,
-  sanitizeOps: false,
-  fn: () => {
-    assert(factory({ dir: "test" }).load());
-    assert(factory({ dir: "test", name: "foo.db" }).load());
+      await t.step({
+        name: "should accept an object with defaults",
+        fn: () => {
+          assert(factory({ dir: "test" }).load());
+          assert(factory({ dir: "test", name: "foo.db" }).load());
+        },
+      });
+    });
   },
 });
