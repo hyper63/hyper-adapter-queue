7d62e37fbb1a9814cd170598820f81e7342f619d
Tyler Hall <tyler@hyper.io>
2023-11-28 19:23:15 -0500

    chore: bump deps and use npm in lieu of skypack

--- dev_deps.js
+++ dev_deps.js
@@ -1,7 +1,7 @@
 // Schema parsing deps
-export { default as validateFactorySchema } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.2.0/packages/core/utils/plugin-schema.ts'
+export { default as validateFactorySchema } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper%40v4.3.1/packages/core/utils/plugin-schema.ts'
 export { queue as validateQueueAdapterSchema } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-port-queue%40v0.3.0/packages/port-queue/mod.ts'
 
 // std lib deps
-export { assert, assertEquals } from 'https://deno.land/std@0.197.0/assert/mod.ts'
-export { createHyperVerify } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-connect%40v0.8.0/packages/connect/deno/mod.ts'
+export { assert, assertEquals } from 'https://deno.land/std@0.208.0/assert/mod.ts'
+export { createHyperVerify } from 'https://raw.githubusercontent.com/hyper63/hyper/hyper-connect%40v0.10.0/packages/connect/deno/mod.ts'
